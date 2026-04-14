03a9162ce620a34501754ea01be5c592644c2f88
Girhantri <girhantri.2005@gmail.com>
2026-04-14 14:30:58 +0700

    chore: clean up comments in web routes file

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController; 
 
-// Route dari komputer kamu
+
 Route::post('/employees', [EmployeeController::class, 'store']);
 
-// Route dari GitHub (milik tim/hasil pull)
+
 Route::get('/', function () {
     return view('welcome');
 });
