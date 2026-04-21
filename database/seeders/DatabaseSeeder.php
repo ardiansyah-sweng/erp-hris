7e3995d5eba8c84b5f3ccca7dc7fd12600092569
M Fahrur Rosi <mfahrurrosi14@gmail.com>
2026-04-21 13:52:58 +0700

    Remove JobRoleSeeder and comment out EmployeeSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([
             JobRoleSeeder::class,
-            EmployeeSeeder::class,
+            // EmployeeSeeder::class,
         ]);
     }
 }
