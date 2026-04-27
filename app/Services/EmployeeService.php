35f15450d996bdc510789d19fe8667b6de85f870
Zetnineteen <satyaadin897@gmail.com>
2026-04-28 06:31:22 +0700

    buat method service showEmployee

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -4,5 +4,34 @@ namespace App\Services;
 
 class EmployeeService
 {
-    //
+    public function showEmployee()
+{
+    // dummy
+    $useDummy = true;
+
+    if ($useDummy) {
+        return collect([
+            [
+                'id' => 1,
+                'name' => 'Budi Santoso',
+                'email' => 'budi@mail.com',
+                'job_role' => 'Cashier'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Siti Aminah',
+                'email' => 'siti@mail.com',
+                'job_role' => 'Manager'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Andi Wijaya',
+                'email' => 'andi@mail.com',
+                'job_role' => 'Admin'
+            ]
+        ]);
+    }
+
+    return Employee::all();
+}
 }
