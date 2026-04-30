83150f37c122e5adc527f336fe0a1f2aa9372fda
M Falih <2300018187@webmail.uad.ac.id>
2026-04-30 18:19:53 +0700

    buat view/edit tanpa controller

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,21 @@ Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+//Edit file dummy ( karna belum ada controller edit dan updatenya)
+Route::get('/employee/test-edit', function () {
+    // Membuat data dummy agar variabel di view tidak error
+    $employee = (object) [
+        'id' => 1,
+        'name' => 'Budi Setiawan',
+        'email' => 'budi@erp-hris.com',
+        'job_role_id' => 1, // ID untuk Cashier 
+        'address' => 'Jl. Merdeka No. 45, Semarang'
+    ];
+
+    return view('employee.edit', compact('employee'));
+});
+// Route dummy untuk menangkap klik tombol Update dari form edit
+Route::put('/employee/test-edit', function () {
+    return "Tombol Update berhasil diklik! (Ini hanya simulasi, data belum tersimpan karena Controller Update asli belum disambungkan).";
+});
