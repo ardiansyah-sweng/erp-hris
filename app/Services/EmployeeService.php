e2074a51a0533f8d831681ce1eb4f35e3aab7768
Ghinan <sanbembe871@gmail.com>
2026-05-05 12:34:07 +0700

    kumpul

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -3,11 +3,36 @@
 namespace App\Services;
 
 use App\Models\Employee;
+use Exception;
 
 class EmployeeService
 {
     public function getAllEmployee()
     {
         return Employee::all();
+    }
+
+    // method kamu ditambah di sini
+    public function destroyEmployee($id)
+    {
+        try {
+            $employee = Employee::findOrFail($id);
+            $employee->delete();
+
+            return [
+                'statusCode' => 200,
+                'message' => 'Employee deleted successfully!',
+                'data' => [
+                    'id' => $employee->id,
+                    'name' => $employee->name
+                ]
+            ];
+        } catch (Exception $e) {
+            return [
+                'statusCode' => 500,
+                'message' => 'Gagal menghapus data karyawan.',
+                'error' => $e->getMessage()
+            ];
+        }
     }
 }
