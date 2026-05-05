3ea7b979c8ddc81929cad3d2f706b69e837cb259
Zetnineteen <satyaadin897@gmail.com>
2026-05-05 16:41:23 +0700

    perbaikan config

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -6,36 +6,38 @@ use App\Models\Employee;
 
 class EmployeeService
 {
-
-    public function showEmployee()
-{
-    // dummy
-    $useDummy = true;
-
-    if ($useDummy) {
-        return collect([
-            [
-                'id' => 1,
-                'name' => 'Budi Santoso',
-                'email' => 'budi@mail.com',
-                'job_role' => 'Cashier'
-            ],
-            [
-                'id' => 2,
-                'name' => 'Siti Aminah',
-                'email' => 'siti@mail.com',
-                'job_role' => 'Manager'
-            ],
-            [
-                'id' => 3,
-                'name' => 'Andi Wijaya',
-                'email' => 'andi@mail.com',
-                'job_role' => 'Admin'
-            ]
-        ]);
+    public function getAllEmployee()
+    {
+        return Employee::all();
     }
+    
+    public function showEmployee()
+    {
+        // dummy
+        $useDummy = true;
 
-    return Employee::all();
-}
-
+        if ($useDummy) {
+            return collect([
+                [
+                    'id' => 1,
+                    'name' => 'Budi Santoso',
+                    'email' => 'budi@mail.com',
+                    'job_role' => 'Cashier'
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Siti Aminah',
+                    'email' => 'siti@mail.com',
+                    'job_role' => 'Manager'
+                ],
+                [
+                    'id' => 3,
+                    'name' => 'Andi Wijaya',
+                    'email' => 'andi@mail.com',
+                    'job_role' => 'Admin'
+                ]
+            ]);
+        }
+        return Employee::all();
+    }
 }
