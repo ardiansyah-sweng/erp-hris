f8e3723cc2a2d9f1328d79f5ae5421c5e3fb8f39
Zetnineteen <satyaadin897@gmail.com>
2026-05-12 14:39:20 +0700

    perbaikan

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -32,31 +32,6 @@ class EmployeeService
     
     public function showEmployee()
     {
-        // dummy
-        $useDummy = true;
-
-        if ($useDummy) {
-            return collect([
-                [
-                    'id' => 1,
-                    'name' => 'Budi Santoso',
-                    'email' => 'budi@mail.com',
-                    'job_role' => 'Cashier'
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'Siti Aminah',
-                    'email' => 'siti@mail.com',
-                    'job_role' => 'Manager'
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Andi Wijaya',
-                    'email' => 'andi@mail.com',
-                    'job_role' => 'Admin'
-                ]
-            ]);
-        }
         return Employee::all();
     }
 
