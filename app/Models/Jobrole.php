620fc8582bb8d1ed01b06cc65c43600feeecd049
C200MOHCSAMRAMADANI <2300018200@webmail.uad.ac.id>
2026-05-12 13:19:38 +0700

    fix: mengembalikan  dan memperbaiki syntax error

--- app/Models/Jobrole.php
+++ app/Models/Jobrole.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class JobRole extends Model
 {
     use HasFactory;
-
+     protected $fillable = [ 'role'];
     /**
      * The table associated with the model.
      *
      * @var string
      */
+    
     protected $table = 'job_roles';
 }
