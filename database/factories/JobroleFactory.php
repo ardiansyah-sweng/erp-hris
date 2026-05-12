9890b66619d67e03fa35785d13b6d94341d8bd9b
ardiansyah-sweng <ardiansyah.2018@outlook.com>
2026-05-12 14:43:59 +0700

    Refactor JobRoleFactory class name and remove unused imports

--- database/factories/JobroleFactory.php
+++ database/factories/JobroleFactory.php
@@ -2,19 +2,10 @@
 
 namespace Database\Factories;
 
-use App\Models\Jobrole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends Factory<Jobrole>
- */
-class JobroleFactory extends Factory
+class JobRoleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
