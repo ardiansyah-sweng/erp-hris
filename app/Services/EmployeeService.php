543b81c05f0a6f7ea06d872986aa3e0a1fcc4325
abdif <abdifikri21@gmail.com>
2026-05-12 14:10:01 +0700

    fix: merge conflict

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Employee;
+use Exception;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
@@ -54,5 +55,37 @@ class EmployeeService
             ];
         }
     }
-}
+     public function createEmployee(array $data): Employee
+    {
+        DB::beginTransaction();
 
+        try {
+            $dateOfBirth = Carbon::parse($data['date_of_birth']);
+            $age = $dateOfBirth->diffInYears(Carbon::now());
+
+            $employee = Employee::create([
+                'name'           => $data['name'],
+                'email'          => $data['email'],
+                'phone_number'   => $data['phone_number'],
+                'place_of_birth' => $data['place_of_birth'],
+                'date_of_birth'  => $data['date_of_birth'],
+                'address'        => $data['address'],
+                'id_number'      => $data['id_number'],
+                'age'            => $age,
+                'role_id'        => $data['role_id'],
+            ]);
+
+            DB::commit();
+
+            return $employee;
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            Log::error('Failed to create employee: ' . $e->getMessage());
+
+            throw $e;
+        }
+    }
+
+}
